d50a968fb3a0f150a1ccfddbb4d100be9b9d88be
essam939 <mohamed.essam9393@gmail.com>
2023-07-10 20:33:22 +0200

    add location

--- lib/modules/home/bloc/location_bloc/location_event.dart
+++
@@ -1,7 +0,0 @@
-part of 'location_bloc.dart';
-
-// Location event
-abstract class LocationEvent {}
-
-// Request location event
-class RequestLocation extends LocationEvent {}
--- lib/modules/home/home_screen.dart
+++ lib/modules/home/home_screen.dart
@@ -1,13 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:intl/intl.dart';
-import 'package:local_auth/local_auth.dart';
 import 'package:riyad/core/app_colors.dart';
 import 'package:riyad/core/app_images.dart';
 import 'package:riyad/core/app_theme.dart';
 import 'package:riyad/modules/attendance/bloc/bloc/daily_attend_bloc.dart';
-import 'package:riyad/modules/home/bloc/clock_cubit.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:riyad/modules/home/components/checkIn_component.dart';
 import 'package:riyad/modules/home/components/checkOut_component.dart';
 import 'package:riyad/modules/home/components/clock_component.dart';
@@ -22,6 +23,9 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late DailyAttendBloc dailyAttendBloc;
+  late StreamSubscription<Position> positionStream;
+  double lat=0.0;
+  double lang=0.0;
   String? userName;
   String? userId;
 
@@ -29,6 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
   void initState() {
     // TODO: implement initState
     super.initState();
+    startListening();
     dailyAttendBloc = DailyAttendBloc();
     dailyAttendBloc.add(GetDaylyAttendance());
     getUsetData();
@@ -42,6 +47,56 @@ class _HomeScreenState extends State<HomeScreen> {
     });
     print("dataUpdated");
   }
+
+
+  void startListening() {
+    // Check if location services are enabled
+    Geolocator.isLocationServiceEnabled().then((serviceEnabled) {
+      if (!serviceEnabled) {
+        // Location services are not enabled, handle it accordingly
+        return;
+      }
+
+      // Check for location permission
+      Geolocator.checkPermission().then((permission) {
+        if (permission == LocationPermission.denied) {
+          Geolocator.requestPermission().then((newPermission) {
+            if (newPermission == LocationPermission.denied) {
+              // Permission is denied, handle it accordingly
+              return;
+            }
+
+            // Start listening to location updates
+            startListeningUpdates();
+          });
+        } else {
+          // Start listening to location updates
+          startListeningUpdates();
+        }
+      });
+    });
+  }
+
+  void startListeningUpdates() {
+    positionStream = Geolocator.getPositionStream(
+    ).listen((Position position) {
+      // Handle the updated position
+      setState(() {
+        lang= position.longitude;
+        lat= position.latitude;
+      });
+      double latitude = position.latitude;
+      double longitude = position.longitude;
+      print('Latitude: $latitude, Longitude: $longitude');
+    });
+  }
+
+  void stopListening() {
+    if (positionStream != null) {
+      positionStream.cancel(); // Cancel the location updates stream
+    }
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -122,14 +177,14 @@ class _HomeScreenState extends State<HomeScreen> {
                       SizedBox(
                         height: 20,
                       ),
-                      const Row(
+                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Icon(Icons.location_on),
                           SizedBox(
                             width: 10,
                           ),
-                          Text("Location : You Are Not In Office Reach"),
+                          Text("Location : $lat:$lang"),
                         ],
                       ),
                       const SizedBox(
